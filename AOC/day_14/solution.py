09d10a96022caa82f5918fdc4b4394f99e803a8b
Panc4kes <jack.sun4@student.education.wa.edu.au>
2020-12-14 15:40:36 +0800

    update

--- AOC/day_14/solution.py
+++ AOC/day_14/solution.py
@@ -6,12 +6,7 @@ with open("input.txt") as f:
             prog[current_mask] = []
         else:
             prog[current_mask].append((int(line.split()[0].partition("[")[2][:-1]), int(line.split()[2])))
-
-max_val = 0
-for mask in prog:
-    for vals in prog[mask]:
-        max_val = vals[0] if max_val < vals[0] else max_val
-mem = [0]*(max_val+1)
+mem = {}
 
 for ins in prog:
     for vals in prog[ins]:
@@ -20,4 +15,5 @@ for ins in prog:
             if mask != "X":
                 mem[vals[0]][i] = mask
 
-nums = [int("".join(num), 2) if num != 0 else 0 for num in mem]
+nums = [int("".join(mem[num]), 2) if mem[num] != 0 else 0 for num in mem]
+print(sum(nums))
